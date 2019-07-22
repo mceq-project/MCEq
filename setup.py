e81bf4d94c43d41a87dc195c1034a3bf5735bda8
Anatoli <afedynitch@gmail.com>
2019-07-22 00:58:03 -0600

    Hotfix build_ext

--- setup.py
+++ setup.py
@@ -2,6 +2,19 @@ import sys
 from os.path import join
 from setuptools import setup, Extension
 
+from distutils.command import build_ext
+
+def get_export_symbols(self, ext):
+    parts = ext.name.split(".")
+    print('parts', parts)
+    if parts[-1] == "__init__":
+        initfunc_name = "PyInit_" + parts[-2]
+    else:
+        initfunc_name = "PyInit_" + parts[-1]
+
+build_ext.build_ext.get_export_symbols = get_export_symbols
+
+
 # Require pytest-runner only when running tests
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
