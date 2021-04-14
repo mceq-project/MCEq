eb42587756f9bd84affecbf1e81190eec79a66aa
Anatoli Fedynitch <afedynitch@gmail.com>
2021-04-14 16:22:42 +0900

    Fixed scipy import when no cupy detected

--- MCEq/__init__.py
+++ MCEq/__init__.py
@@ -2,22 +2,23 @@ import mceq_config as config
 
 if config.kernel_config.lower() == "cuda":
     import cupy
+    import cupyx
     zeros = cupy.zeros
     ones = cupy.ones
     eye = cupy.eye
     diag = cupy.diag
-    csr_matrix = cupy.sparse.csr_matrix
+    csr_matrix = cupyx.scipy.sparse.csr_matrix
     linalg = cupy.linalg
     asarray = cupy.asarray
     floatlen = config.floatlen
 else:
     import numpy as np
-    import scipy as sp
+    from scipy.sparse import csr_matrix
     zeros = np.zeros
     ones = np.ones
     eye = np.eye
     diag = np.diag
-    csr_matrix = sp.sparse.csr_matrix
+    csr_matrix = csr_matrix
     linalg = np.linalg
     asarray = np.asarray
     floatlen = config.floatlen
